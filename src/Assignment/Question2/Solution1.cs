41d5351b0bbc266e8662d8704ed38e3cb649e24c
iivchenko <iivchenko@live.com>
2023-07-29 14:05:14 +0200

    Implement Solution 1 for Question 2
    Make each test self containing (e.g. code, methods)
    Add  Reverse with loop forwards

--- src/Assignment/Question2/Solution1.cs
+++ src/Assignment/Question2/Solution1.cs
@@ -4,13 +4,42 @@ public sealed class Solution1
 {
     [Theory]
     [MemberData(nameof(GetDataSource))]
-    public void Run_WithLoops(IEnumerable<int> input, IEnumerable<int> expected)
+    public void Run_WithLoop(IEnumerable<int> input, IEnumerable<int> expected)
     {
         // Arrange
         var list = new LinkedList<int>(input);
 
         // Act
-        var actual = ReverseWithLoop(list);
+        var actual = new LinkedList<int>();
+
+        foreach (var item in list)
+        {
+            actual.AddFirst(item);
+        }
+
+        // Assert
+        actual
+            .Should()
+            .Equal(expected);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetDataSource))]
+    public void Run_WithLoopForward(IEnumerable<int> input, IEnumerable<int> expected)
+    {
+        // Arrange
+        var list = new LinkedList<int>(input);
+
+        // Act
+        var actual = new LinkedList<int>();
+        var current = list.First;
+
+        while (current != null)
+        {
+            actual.AddFirst(current.Value);
+
+            current = current.Next;
+        }
 
         // Assert
         actual
@@ -24,17 +53,5 @@ public sealed class Solution1
         yield return new[] { new[] { 7 }, new[] { 7 } };
         yield return new[] { new[] { 7, 13 }, new[] { 13, 7 } };
         yield return new[] { new[] { 7, 13, 666, 69, 42 }, new[] { 42, 69, 666, 13, 7 } };
-    }
-
-    private static LinkedList<T> ReverseWithLoop<T>(LinkedList<T> list)
-    {
-        var newList = new LinkedList<T>();
-
-        foreach (var item in list)
-        {
-            newList.AddFirst(item);
-        }
-
-        return newList;
     }
 }
