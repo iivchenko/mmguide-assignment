ed8f7063f76bc26cf4109901bb7681439a7431e9
iivchenko <iivchenko@live.com>
2023-07-29 13:55:04 +0200

    Implement Solution 1 for Question 2
    Add Reverse with loop

--- src/Assignment/Question2/Solution1.cs
+++ src/Assignment/Question2/Solution1.cs
@@ -2,4 +2,39 @@
 
 public sealed class Solution1
 {
+    [Theory]
+    [MemberData(nameof(GetDataSource))]
+    public void Run_WithLoops(IEnumerable<int> input, IEnumerable<int> expected)
+    {
+        // Arrange
+        var list = new LinkedList<int>(input);
+
+        // Act
+        var actual = ReverseWithLoop(list);
+
+        // Assert
+        actual
+            .Should()
+            .Equal(expected);
+    }
+
+    public static IEnumerable<object[]> GetDataSource()
+    {
+        yield return new[] { Array.Empty<int>(), Array.Empty<int>() };
+        yield return new[] { new[] { 7 }, new[] { 7 } };
+        yield return new[] { new[] { 7, 13 }, new[] { 13, 7 } };
+        yield return new[] { new[] { 7, 13, 666, 69, 42 }, new[] { 42, 69, 666, 13, 7 } };
+    }
+
+    private static LinkedList<T> ReverseWithLoop<T>(LinkedList<T> list)
+    {
+        var newList = new LinkedList<T>();
+
+        foreach (var item in list)
+        {
+            newList.AddFirst(item);
+        }
+
+        return newList;
+    }
 }
